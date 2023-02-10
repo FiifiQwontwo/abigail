ae433e29e92e1e8b576752d766f985b48b958e71
FiifiQwontwo <quophi555@gmail.com>
2023-02-10 18:37:25 +0000

    created a multiuser models and yet to test

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,16 +6,8 @@ from django.contrib import admin
 BaseUser = get_user_model()
 
 
-class EKKPastorAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name',)
-    ordering = ('last_name',)
 
 
-class BusscellAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name',)
-    ordering = ('last_name',)
-
-
-admin.site.register(EKKPastor, EKKPastorAdmin)
-admin.site.register(Busscell, BusscellAdmin)
+admin.site.register(EKKPastor)
+admin.site.register(Busscell)
 admin.site.register(BaseUser)
