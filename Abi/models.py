dc64db61ae74698b6477b57d479ac956c74253a3
FiifiQwontwo <quophi555@gmail.com>
2023-02-10 23:13:31 +0000

    created member_type models

--- Abi/models.py
+++ Abi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class MemberType(models.Model):
+    member_type = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
