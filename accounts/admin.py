e4db6814eeacf03063fc7655c09b61bf7370fbe0
FiifiQwontwo <quophi555@gmail.com>
2023-02-10 12:56:32 +0000

    created a multiuser models and yet to test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from models import EKKPastor, Busscell
 from django.contrib import admin
 
-# Register your models here.
+BaseUser = get_user_model()
+
+
+class EKKPastorAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+
+
+class BusscellAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+
+
+admin.site.register(EKKPastor, EKKPastorAdmin)
+admin.site.register(Busscell, BusscellAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,37 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
 # User accounts Manager
-class MyAccountManager(BaseUserManager):
-    def create_user(self, last_name, username, email, phone, password=None):
-        if not email:
-            raise ValueError('User must have an email address')
-        if not username:
-            raise ValueError("User must have a username")
 
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            # first_name=first_name,
-            last_name=last_name,
-            phone=phone,
-        )
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, last_name, email, username, phone, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
-            # first_name=first_name,
-            last_name=last_name,
-            phone=phone
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(email, password=password, **extra_fields)
 
-        )
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
@@ -40,12 +28,9 @@ class MyAccountManager(BaseUserManager):
         return user
 
 
-class Account(AbstractBaseUser):
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(max_length=50, unique=True)
-    phone = models.CharField(max_length=50)
+class BaseUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=100, unique=True)
     # required Fields
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
@@ -59,7 +44,7 @@ class Account(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'last_name', 'phone']
     #  u will always need to end the class with() or natural key error awaits
-    objects = MyAccountManager()
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
@@ -69,3 +54,19 @@ class Account(AbstractBaseUser):
 
     def has_module_perms(self, add_label):
         return True
+
+
+class EKKPastor(BaseUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+
+class Busscell(BaseUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
